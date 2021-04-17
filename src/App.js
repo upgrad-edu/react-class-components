471d512a3a5b3f0673d840c04f0424d4013b9a9b
Ashita Jain <Ashita.Jain@upgrad.local>
2021-04-17 15:41:18 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ function App() {
   return (
     <div>
       Phone Directory
+      <div className="header">
+      	Phone Directory
+      </div>
+      <button>Add</button>
+      <div>
+      	<span>Name</span><br />
+       	<span>Phone</span>
+      </div>
     </div>
   );
 }
