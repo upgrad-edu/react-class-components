15241265a0b57966a0e6c733af296e625b509b21
Ashita Jain <Ashita.Jain@upgrad.local>
2021-04-19 15:08:51 +0530

    Styling Header: Directly Writing Style Alongside JSX

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const Header = function() {
    return (
-       <div className="header">
+       <div style={{textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase'}}>
          Phone Directory
        </div>
    )
