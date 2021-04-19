cd84fcb2df2f55d24ea2320be6c9acc0c0ae20d7
Ashita Jain <Ashita.Jain@upgrad.local>
2021-04-19 15:02:52 +0530

    Creating Header As a Class Component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Header extends Component {
+   render() {
+       return (
+           <div className="header">
+               Phone Directory
+           </div>
+       )
+    }
+}
+
+export default Header;
