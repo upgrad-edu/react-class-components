c90533fa2a33586030bfe6923f766dce1123cdc6
Ashita Jain <Ashita.Jain@upgrad.local>
2021-04-20 19:39:16 +0530

    Adding More Details to the Form on Add Subscriber Page

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -7,12 +7,23 @@ class AddSubscriber extends Component {
   render() {
     return (
         <div className="component-container">
+
             <Header heading="Add Subscriber"/>
             <div className="component-body-container">
                 <button className="custom-btn">Back</button>
+
                 <form className="subscriber-form">   
                     <label htmlFor="name" className="label-control">Name:</label><br />
                     <input id="name" type="text" className="input-control" name="name" /><br /><br />
+                    <label htmlFor="phone" className="label-control">Phone:</label><br />
+                    <input id="phone" type="text" className="input-control" name="phone" /><br /><br />
+                
+                    <div className="subscriber-info-container">
+                        <span className="subscriber-to-add-heading">Subscriber to be added:</span><br />
+                        <span className="subscriber-info">Name: </span><br />
+                        <span className="subscriber-info">Phone: </span>
+                    </div>
+
                 </form>
             </div>
         </div>
