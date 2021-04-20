603b38b927625b84e673593a3d1da25bbc85437d
Ashita Jain <Ashita.Jain@upgrad.local>
2021-04-20 19:31:08 +0530

    Adding Form On AddSubscriber Page

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -9,6 +9,10 @@ class AddSubscriber extends Component {
             <Header heading="Add Subscriber"/>
             <div className="component-body-container">
                 <button className="custom-btn">Back</button>
+                <form className="subscriber-form">   
+                    <label htmlFor="name" className="label-control">Name:</label><br />
+                    <input id="name" type="text" className="input-control" name="name" /><br /><br />
+                </form>
             </div>
         </div>
     )
