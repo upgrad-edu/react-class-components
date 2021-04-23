2eac045ab5dcf50ab308d581cf407dc6e160e750
Ashita Jain <Ashita.Jain@upgrad.local>
2021-04-23 19:52:59 +0530

    Adding addSubscriberHandler Method To The PhoneDirectory Component

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -8,6 +8,18 @@ class PhoneDirectory extends Component {
            subscribersList: []
         };
     }
+
+    addSubscriberHandler = (newSubscriber) => {
+        let subscribersList = this.state.subscribersList;
+        if (subscribersList.length > 0) {
+            newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
+        } else {
+          newSubscriber.id = 1;
+        }
+        subscribersList.push(newSubscriber);
+        this.setState({ subscribersList: subscribersList });
+        console.log(this.state.subscribersList);
+    }
     
     render() {
        return (
