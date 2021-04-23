be2525778824c4fbeda8b4ca1fb0fd343575e853
Ashita Jain <Ashita.Jain@upgrad.local>
2021-04-23 19:49:05 +0530

    Adding Constructor And State To The PhoneDirectory Component

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
 class PhoneDirectory extends Component {
-   render() {
+    constructor() {
+        super();
+     
+        this.state = {
+           subscribersList: []
+        };
+    }
+    
+    render() {
        return (
            <AddSubscriber />
        )
