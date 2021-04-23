06a7fe3849852437153a02287109562fc845d0b3
Ashita Jain <Ashita.Jain@upgrad.local>
2021-04-23 19:46:16 +0530

    Creating PhoneDirectory Component

---
+++ src/PhoneDirectory.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PhoneDirectory extends Component {
+   render() {
+       return (
+           <AddSubscriber />
+       )
+   }
+}
+
+export default PhoneDirectory;
